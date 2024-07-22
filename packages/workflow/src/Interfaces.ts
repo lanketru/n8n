2836d0e06a57e6509d4e4234aa3f9e40a0b47b0f
Danny Martini <danny@n8n.io>
2024-07-22 17:18:04 +0200

    --wip--
    --wip--
    
    --wip--
    
    --wip--
    
    clean up
    
    use spaces in package.json

--- packages/workflow/src/Interfaces.ts
+++ packages/workflow/src/Interfaces.ts
@@ -2075,6 +2075,7 @@ export interface ISourceData {
 
 export interface StartNodeData {
 	name: string;
+	// TODO: What is this for?
 	sourceData: ISourceData | null;
 }
 
@@ -2212,14 +2213,14 @@ export interface IWorkflowExecuteAdditionalData {
 }
 
 export type WorkflowExecuteMode =
-	| 'cli'
-	| 'error'
+	| 'cli' // unused
+	| 'error' // unused, but maybe used for error workflows
 	| 'integrated'
 	| 'internal'
 	| 'manual'
-	| 'retry'
-	| 'trigger'
-	| 'webhook';
+	| 'retry' // unused
+	| 'trigger' // unused
+	| 'webhook'; // unused
 
 export type WorkflowActivateMode =
 	| 'init'
