56dbfaf47e4df1523fc769c5c3ddd61a2bec55f0
Danny Martini <danny@n8n.io>
2024-07-31 11:08:42 +0200

    allow switching between old and new partial execution using client side local storage

--- packages/cli/src/interfaces.ts
+++ packages/cli/src/interfaces.ts
@@ -477,6 +477,7 @@ export interface IWorkflowExecutionDataProcess {
 	workflowData: IWorkflowBase;
 	userId?: string;
 	projectId?: string;
+	partialExecutionVersion?: string;
 }
 
 export interface IWorkflowExecuteProcess {
